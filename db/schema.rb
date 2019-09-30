8e185dcfea0963dc80c5a1c306add0bd9e062012
stephaniegiang <stephaniegiang@github.com>
2019-09-29 20:04:17 -0400

    Add verificaton code to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_231432) do
+ActiveRecord::Schema.define(version: 2019_09_30_000335) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_29_231432) do
     t.string "phone_number"
     t.string "first_name"
     t.string "last_name"
+    t.string "verification_code"
   end
 
 end
